ebf49ebe3a8294c3073bbf268353ab20b0c09eaf
ASUS TUF <erkinov.0021@gmail.com>
2026-03-29 12:50:00 +0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Taklifnoma from "./Taklifnoma/Taklifnoma";
 
 export default function App() {
